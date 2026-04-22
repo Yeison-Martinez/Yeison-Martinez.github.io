4e5e94951e3db43080576c55c852252454e874cc
Yeison-Martinez <133739273+Yeison-Martinez@users.noreply.github.com>
2026-04-22 18:49:40 +0000

    Comentar sección de total plástico en HTML y ajustar cálculo de total plástico en JavaScript

--- js/main_mundoMagico.js
+++ js/main_mundoMagico.js
@@ -194,11 +194,13 @@ function calc() {
   ttlbill.innerHTML = `$ ${ttlbills_s}`;
 
   // TOTAL PLASTICO
+  var ttl_deb = 0;
+  var ttl_cre = 0;
   var ttl_deb = parseFloat(document.getElementById("tarj_deb--vlr").value);
   var ttl_cre = parseFloat(document.getElementById("tarj_cre--vlr").value);
   var sum_plas = ttl_deb + ttl_cre;
   var sum_plas_s = Separador(sum_plas);
-  ttl_plas.innerHTML = `$ ${sum_plas_s}`;
+  // ttl_plas.innerHTML = `$ ${sum_plas_s}`;
 
   // TOTAL BOLETAS VLR UNO
   var cant22k = parseFloat(document.getElementById("cant22k").value);
